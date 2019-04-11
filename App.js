901a1dfdf4b42c632d5266c16d8ba64c7d9b4b4a
Vlademir Junior <vlademirjunior@protonmail.com>
2019-04-11 01:05:08 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import Item from './src/Item'
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lista: [],
+      input: "",
+      netstatus: 0
+    };
+
+    this.url = "https://b7web.com.br/todo/49833";
+
+    this.loadLista = this.loadLista.bind(this);
+    this.addButton = this.addButton.bind(this);
+
+    this.loadLista();
+  }
+
+  loadLista() {
+    fetch(this.url)
+      .then((r) => r.json())
+      .then((json) => {
+        let state = this.state;
+        state.lista = json.todo;
+        this.setState(state);
+      });
+  }
+
+  addButton() {
+    let texto = this.state.input;
+    let state = this.state;
+    state.input = "";
+    this.setState(state);
+
+    fetch(this.url, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        item: texto
+      })
+    }).then((r) => r.json())
+      .then((json) => {
+        alert("Item inserido com suceso!");
+        this.loadLista();
+      });
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.addArea}>
+          <Text style={styles.addTxt}>Adicione uma nova tarefa</Text>
+          <TextInput style={styles.input} onChangeText={(text) => {
+            let state = this.state;
+            state.input = text;
+            this.setState(state);
+          }} value={this.state.input} />
+          <Button title="Adicionar" onPress={this.addButton} />
+        </View>
+        <FlatList
+          data={this.state.lista}
+          renderItem={({ item }) => <Item data={item} url={this.url} loadFunction={this.loadLista} />}
+          keyExtractor={(item, index) => item.id}
+        />
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  addArea: {
+    marginBottom: 20,
+    backgroundColor: '#DDD',
+  },
+  addTxt: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginBottom: 10,
+    marginTop: 10
+  },
+  input: {
+    height: 40,
+    backgroundColor: '#CCC',
+    marginLeft: 20,
+    marginRight: 20,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  statusView: {
+    height: 50,
+    backgroundColor: '#EEE'
+  }
+});
---
+++ src/Item.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
+
+export default class Item extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            done: (this.props.data.done == '1' ? styles.done : styles.undone),
+        };
+
+        this.marcar = this.marcar.bind(this);
+        this.excluir = this.excluir.bind(this);
+    }
+
+    excluir() {
+        fetch(this.props.url + '/' + this.props.data.id, {
+            method: "DELETE",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        }).then((r) => r.json())
+            .then((json) => {
+                alert("Item exclúido com suceso!");
+                this.props.loadFunction();
+            });
+    }
+
+    marcar() {
+        let state = this.state;
+
+        let done = 'sim';
+
+        if (state.done == styles.undone) {
+            state.done = styles.done;
+            done = 'sim';
+        } else {
+            state.done = styles.undone;
+            done = 'nao';
+        }
+
+        fetch(this.props.url + '/' + this.props.data.id, {
+            method: "PUT",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                done: done
+            })
+        }).then((r) => r.json())
+            .then((json) => {
+                alert("Item atualizado com suceso!");
+            });
+
+        this.setState(state);
+    }
+
+    render() {
+        return (
+            <View style={styles.area}>
+                <TouchableHighlight style={[styles.marcarArea, this.state.done]} onPress={this.marcar}>
+                    <View>
+
+                    </View>
+                </TouchableHighlight>
+                <Text>{this.props.data.item}</Text>
+                <Button style={styles.botao} title="X" onPress={this.excluir} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    area: {
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderBottomWidth: 1,
+        borderColor: '#CCC',
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    marcarArea: {
+        width: 40,
+        height: 40,
+        marginRight: 10,
+        marginLeft: 10,
+    },
+    undone: {
+        backgroundColor: '#CCC'
+    },
+    done: {
+        backgroundColor: '#0F0'
+    },
+    botao: {
+        width: 40,
+        height: 40
+    }
+
+});
